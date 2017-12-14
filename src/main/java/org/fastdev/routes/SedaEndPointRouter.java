b3a62bee8e7c00474700c2900c63e8ccfb6dc574
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2017-12-14 10:53:55 -0500

    SedaEndPointRouter
    SedaEndPointRouter

---
+++ src/main/java/org/fastdev/routes/SedaEndPointRouter.java
@@ -0,0 +1,22 @@
+package org.fastdev.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class SedaEndPointRouter extends RouteBuilder{
+    public static final String SEDA_START = "seda:start";
+    public static final String SEDA_END = "seda:End";
+
+
+    @Override
+    public void configure() throws Exception {
+        from(SEDA_START).routeId("StartRouteId")
+                .setBody().simple("Start Message")
+                .to(SEDA_END)
+                .log("Message at start route completion = ${body}");
+
+
+        from(SEDA_END).routeId("EndRouteId")
+                .setBody().simple("End Message")
+                .log("message after end-router completion = ${body}");
+    }
+}
