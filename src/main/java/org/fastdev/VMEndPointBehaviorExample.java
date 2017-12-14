ffd6f9f7e89c0f83104d8da553be384529f8ce9a
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2017-12-14 11:02:46 -0500

    VMEndPointBehaviorExample
    VMEndPointBehaviorExample

---
+++ src/main/java/org/fastdev/VMEndPointBehaviorExample.java
@@ -0,0 +1,25 @@
+package org.fastdev;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.fastdev.routes.VMEndPoint1stRouter;
+import org.fastdev.routes.VMEndPoint2ndRouter;
+
+public class VMEndPointBehaviorExample {
+
+    public static void main(String[] args) throws Exception {
+        CamelContext firstCamelContext = new DefaultCamelContext();
+        firstCamelContext.addRoutes(new VMEndPoint1stRouter());
+        firstCamelContext.start();
+
+        CamelContext secondCamelContext = new DefaultCamelContext();
+        secondCamelContext.addRoutes(new VMEndPoint2ndRouter());
+        secondCamelContext.start();
+
+        ProducerTemplate producerTemplate=firstCamelContext.createProducerTemplate();
+        producerTemplate.sendBody(VMEndPoint1stRouter.VM_START, "Initial Message");
+
+        Thread.sleep(2000);
+    }
+}
